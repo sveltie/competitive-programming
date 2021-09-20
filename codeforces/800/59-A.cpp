ab216af977cafaa80e9d97092ad8f49e5c7fced3
Svelt <83514357+Yukii2k@users.noreply.github.com>
2021-09-20 19:21:10 +0700

    (submission): Submit 59 - A

---
+++ codeforces/800/59-A.cpp
@@ -0,0 +1,39 @@
+/*  Codeforces problem set 59 - A https://codeforces.com/contest/59/problem/A
+    Using WSL Ubuntu 20.04.3 LTS
+    GNU C++17  
+    
+    Time : 62 ms, Memory : 3700 KB  */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    string s, s_upper, s_lower, res;
+ 
+    cin >> s;
+ 
+    int N = s.size();
+ 
+    for (int i = 0; i < N; i++) {
+        if (isupper(s[i])) {
+            s_upper += s[i];
+        } else {
+            s_lower += s[i];
+        }
+    }
+ 
+    for (int j = 0; j < N; j++) {
+        if (s_upper.size() > s_lower.size()) {
+            res += toupper(s[j]);
+        } else {
+            res += tolower(s[j]);
+        }
+    }
+ 
+    cout << res << endl;
+ 
+    return 0;
+}
