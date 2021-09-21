08ff9124ed9f283c5dac2b257442bed08e7196a7
Svelt <83514357+Yukii2k@users.noreply.github.com>
2021-09-21 08:02:47 +0700

    (submission): Submit 1560 - B

---
+++ codeforces/800/1560B.cpp
@@ -0,0 +1,39 @@
+/*  Codeforces problem set 1560 - B https://codeforces.com/contest/1560/problem/B
+    Using WSL Ubuntu 20.04.3 LTS
+    GNU C++17 7.3.0
+    
+    Time : 93 ms, Memory : 3700 KB  */
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t;
+    cin >> t;
+ 
+    int a, b, c;
+    
+    for (int i = 0; i < t; i++) {
+        cin >> a >> b >> c;
+ 
+        int len = 2 * (abs(a - b) - 1) + 2;
+ 
+        if (len < max(a, b) || len < c) {
+            cout << -1 << endl;
+        } else {
+            int res;
+            res = c + abs(a - b);
+            
+            if (res > abs(a - b) * 2) {
+                res = c - abs(a - b);
+            }
+            
+            cout << res << endl;
+        }
+    }
+    
+    return 0;
+}
